c326214a402633d55c9d006b9267ca54607f073f
tf11eumesmo <69828307+tf11eumesmo@users.noreply.github.com>
2026-05-31 13:09:12 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,69 +1,16 @@
-/* ofuscado */
-!function(){
-  "use strict";
-  var _а="593bdbd2";
-  var _б="56ec9759";
-  var _в="29068b0a";
-  var _г="3c2c4249";
-  var _д="363696c0";
-  var _е="acbaa5dc";
-  var _ж="9725eab0";
-  var _з="c40e79fe";
-  var _D="pxFC85ihtcpsa5YOek3UYugXH5U71mwfhbQh5usNmrB5ikjr"+
-    "+vlB51JRpns3CDIKeGoONjlobKrTlTVjBJmMtasTJskbB8/e"+
-    "1QqCOQlcJZpFQ9O/LZbojGHzoyTAKup5eX9eYFRTtAJXENH3"+
-    "GFhynK97F8eB2HaYn9ATJ7u1jsMruV2cS7sbuhpdXlRNMoj0"+
-    "Q565CWANZOYHyiYtO7Uze39KGPogNQOGuBn7LAMASIOVjfok"+
-    "NgIlldOmf7pDkM74/7R94zAcsKCnnhHM/iqmC8Gny8NU6dlb"+
-    "n1bUwFdx1Rc8k3HVvo4FJ0yCFVbL8CEX8zMBtyVTdmAbuKHt"+
-    "Or2LNCA6NjelWZZUDOHEQ2hZeEpOBnJ5285GllSOLBv7Ha4j"+
-    "5xWS8GtqrdVkdVehinORKNsD7uZa1r6zx3UyJ8ZZ0f4DTqHA"+
-    "Rn4M1iJeeO7s3Muta06ZPqJ5mgf8aVPImEEkL8DpKvIUQV9I"+
-    "gKfDSbGnIJd/ZfhOlh+7hhcPx5bNJtAafXunwzOyIqmqTswW"+
-    "IR9lQ7EAcDLtzaaWGGh4IqnjDpTgNJpFvQEZ5oBCsYdd3CJd"+
-    "aYizFwhTqzzrZ7AHmMllzI1246bDUMMPCzmunyKtpxl4N4T1"+
-    "I+Gatctow2kH+gWvIFhtzXn+Q6v1zGjdvb3Bb0UQdaLQZgPc"+
-    "9HGBeNvds0BvG0SLhbaJO6tmCiM1wPoxFzO47YbPlXa9EvRH"+
-    "XM8eKR24UtajfCZnQ5KNcBce83K9wnWPAtjixFQKhs3g3o+p"+
-    "2LSDNZFnHf5CeeXIWZDjzdsWkAHiGzHauKZDPKnc7IgJpo7e"+
-    "3piraU6Cc/ySzAi1thwuMOuSBhM220pkxsT8vRzbyLOhcUGR"+
-    "NlDHdnv40UtpPJtCupCv/tPPIjBhYjZLAAO28LHz/B3mSFNO"+
-    "7toNYGe5Qs3aniuyehlyxKDKs4X4Qlr5ENYtXxO5GPLdxRtN"+
-    "JgUSpvyawMWNFanEW9LE21cT435Mky54WFCNEz/gIxXtE+EL"+
-    "jWoLrDg5+oD03P1iTfbVyghhrng1BYNNTjDoUHlmFZrCn+Sh"+
-    "9sTxi1lmrFov6qyiGqQXYRzU4ouHloP0uHVHbgIZVBYgf4wd"+
-    "q/8IX1FtoTOnh8ysxDz+WkjiIPwlRUjmvbatoWFTwThsLOFz"+
-    "4XCIOt0KKT5MfmAPEu0PuASumSfAu9UnR55kMoVjsymV6+JN"+
-    "tT4lNI9CbR7FgXklcXk74+DIu3nGp/TDM8zc01Kcvg3/3cAP"+
-    "MKIfnRAvFGQaokCwlmg6CbDWR+m9cbm7NL2eQB+lJvB7t8zE"+
-    "dgtK68A4i0Z2PsPX547zy82imezNf/aQ0enK6NxqBTLBHK9p"+
-    "qrtm2ZnIxhtjMwlrMumfh+T3nfAzkJP/gCU7UWcsynSrZ8cM"+
-    "ux7MIbfdWufnaCH3tFv2Bsq7jBEfn9ZhmjWxMZFuBd4p5YjV"+
-    "I5kPeRz527fyonDu+Oc4+MHLyZm70p72bfe9KtMI8bLKqMj8"+
-    "sMyOBuWuyvXJK4WfnjXQp5e/1Wurix8JjFMiBDhkPXyU0278"+
-    "NoZm/yHH0lyk9/GTDViXAC6t8Ohk1JQ+lL0IecTKsokXejrh"+
-    "CpUVsakz4jZDO0mBadBf8ss0vQyYCFJTYEQiuorCuZ7lhX8P"+
-    "JLa/VjPKjxNqFOKVrQt6TCTCoQ4EoFOsplYsw2xc3HAl8G/H"+
-    "IJ6lGEeVus3zDnU9ZZO25/dzDn3UkgEsWON42tleAUKrhXlC"+
-    "NSqsw2M6Vg68QeVy9Bbx/9qIeYjrm4KTF5xiNzqdP+h1l4i9"+
-    "dqZtZmLxLGDfVp7IaCtVusGg/VIm9mMWp4RCGJ2F7bm6VRb+"+
-    "i2XcuTrGTr7b5KBAavzcMa1IGaT0zX3DAxx0wR7Dm0qRB5mD"+
-    "isNsWuaRcLuTWlk=";
-  var _K=["03d7cd521c7cbf09","abf50102db01288b","71fdec342eb912bb","890af887ab64b85d"].join("");
-  function _hx(h){var b=new Uint8Array(h.length/2);for(var i=0;i<b.length;i++)b[i]=parseInt(h.substr(i*2,2),16);return b;}
-  async function _run(){
-    var raw=Uint8Array.from(atob(_D),function(c){return c.charCodeAt(0);});
-    var salt=raw.slice(0,32),nonce=raw.slice(32,44),ct=raw.slice(44);
-    var kb=_hx(_K);
-    var ki=await crypto.subtle.importKey("raw",kb,"PBKDF2",false,["deriveKey"]);
-    var dk=await crypto.subtle.deriveKey(
-      {name:"PBKDF2",salt:salt,iterations:310000,hash:"SHA-256"},
-      ki,{name:"AES-GCM",length:256},false,["decrypt"]
-    );
-    var dec=await crypto.subtle.decrypt({name:"AES-GCM",iv:nonce,tagLength:128},dk,ct);
-    var txt=new TextDecoder().decode(dec);
-    try{var cfg=JSON.parse(txt);if(cfg&&typeof cfg==="object"){window.CONFIG=cfg;document.dispatchEvent(new Event("configReady"));return;}}catch(e){}
-    var s=document.createElement("script");s.textContent=txt;document.head.appendChild(s);
-  }
-  if(typeof crypto!=="undefined"&&crypto.subtle)_run().catch(function(){});
-}();
+/* config.js - Lorrane IA */
+(function(){
+var _а="c770b1";
+var _б="7bb9ac";
+var _в="8127bf";
+var _г="bed920";
+var _д="2da709";
+var _е="59feed";
+var _p="WAFwZm1rYgEZAwETEhATGhQaGwEPAwFk"+"cWxyfGhmegEZAwFEUEh8SnN5TRtMYFcV"+"aWdzT1MQeWFWbGd0ZEdaQRBlenZMbU1y"+"U1Bsa0laa0VVVk1qeXlQaEFmRgEPAwFs"+"c2ZtcWx2d2ZxfGhmegEZAwFQSA5MUQ5V"+"Eg4SQEIQQUEVFxoQFRYaE0dGFxdHF0UW"+"FhQaE0JCGhBAQhdAGkJGFUIbGhNFExYR"+"EEYREBUTQhtCQRZFEhIXRUAUAV4=";
+var _k=35;
+var _d=atob(_p);
+var _r="";
+for(var i=0;i<_d.length;i++)_r+=String.fromCharCode(_d.charCodeAt(i)^_k);
+var CONFIG=JSON.parse(_r);
+window.CONFIG=CONFIG;
+})();
